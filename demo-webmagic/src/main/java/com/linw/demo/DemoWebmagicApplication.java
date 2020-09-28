7e65be2d28826185f1688fb16c49f4b7d04d64dc
linwen <linwen@yandui.com>
2020-09-28 23:01:50 +0800

    u

---
+++ demo-webmagic/src/main/java/com/linw/demo/DemoWebmagicApplication.java
@@ -0,0 +1,30 @@
+/*
+package com.linw.demo;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.ServletComponentScan;
+
+import java.util.TimeZone;
+
+@SpringBootApplication
+
+*/
+/**
+Servlet、Filter、Listener可以直接通过@WebServlet、@WebFilter、@WebListener注解自动注册，无需其他代码。
+ *//*
+
+@ServletComponentScan
+@Slf4j
+public class DemoWebmagicApplication {
+
+    public static void main(String[] args) {
+
+        log.info("test");
+
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Shanghai"));
+
+        SpringApplication.run(DemoWebmagicApplication.class, args);
+    }
+}*/
--- demo-webmagic/src/main/java/com/linw/demo/webmagic/action/TestPageProcessor.java
+++ demo-webmagic/src/main/java/com/linw/demo/webmagic/action/TestPageProcessor.java
@@ -16,16 +16,11 @@ public class TestPageProcessor implements PageProcessor {
     // process是定制爬虫逻辑的核心接口，在这里编写抽取逻辑
     public void process(Page page) {
         // 部分二：定义如何抽取页面信息，并保存下来
-        page.putField("author", page.getUrl().regex("https://github\\.com/(\\w+)/.*").toString());
-        page.putField("name", page.getHtml().xpath("//h1[@class='entry-title public']/strong/a/text()").toString());
-        if (page.getResultItems().get("name") == null) {
-            //skip this page
-            page.setSkip(true);
-        }
-        page.putField("readme", page.getHtml().xpath("//div[@id='readme']/tidyText()"));
+        page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/\\w+/\\w+)").all());
+        page.putField("a", page.getHtml().toString());
 
         // 部分三：从页面发现后续的url地址来抓取
-        page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/[\\w\\-]+/[\\w\\-]+)").all());
+        //page.addTargetRequests(page.getHtml().links().regex("(https://github\\.com/[\\w\\-]+/[\\w\\-]+)").all());
     }
 
     @Override
@@ -36,9 +31,8 @@ public class TestPageProcessor implements PageProcessor {
     public static void main(String[] args) {
 
         Spider.create(new GithubRepoPageProcessor())
-                //从"https://github.com/code4craft"开始抓
-                .addUrl("http://www.creprice.cn/ranklist/eyJkaSI6InByb3ZpbmNlIiwicGkiOiIiLCJjaSI6IiIsInB0IjoxMSwidHkiOiJyZW50IiwidW4iOiJjaXR5IiwiaW4iOiJQcmljZSIsIm1uIjoiMjAxOS0wOSJ9.html")
-                .addPipeline(new JsonFilePipeline("D:\\webmagic\\"))
+                .addUrl("https://www.creprice.cn/ranklist/eyJkaSI6ImFsbCIsInBpIjoiY2hvbmdxaW5nIiwiY2kiOiJjcSIsInB0IjoxMSwidHkiOiJzYWxlIiwidW4iOiJkaXN0cmljdCIsImluIjoiUHJpY2UiLCJtbiI6IjIwMjAtMDgifQ==.html")
+                .addPipeline(new JsonFilePipeline("E:\\webmagic\\"))
                 //开启5个线程抓取
                 .thread(5)
                 //启动爬虫
